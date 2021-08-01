f512aa5602a7245182ecff2ba863f1c47d18ec95
Brian nguyen <brian.ng142957@gmail.com>
2021-08-01 11:49:47 +0700

    Add status for bookings

--- booking/booking.go
+++ booking/booking.go
@@ -9,35 +9,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type MongoInstance struct {
-	Client *mongo.Client
-	Db     *mongo.Database
-}
-
-var mg MongoInstance
-
-const (
-	Active   = "active"
-	InActive = "inactive"
-	dbName   = "pttrainer"
-	mongoURI = "mongodb+srv://pttrainer:NmSJWnnBmApV5sEu@maincluster.gkfe6.mongodb.net/pttrainer?authSource=admin&replicaSet=atlas-u555p7-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
-	// mongoURI = "mongodb://localhost:27017/" + dbName
-)
-
-type Booking struct {
-	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	ClientID   primitive.ObjectID `json:"bClientId,omitempty" bson:"bClientId,omitempty"`
-	TimeSlotID primitive.ObjectID `json:"bTimeSlotId,omitempty" bson:"bTimeSlotId,omitempty"`
-	CreatedAt  time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt  time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
-}
 
 func Connect() error {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
@@ -89,6 +64,7 @@ func CreateBooking(ctx context.Context, booking *Booking) (*Booking, error) {
 
 		booking.CreatedAt = time.Now().UTC()
 		booking.UpdatedAt = time.Now().UTC()
+		booking.Status = bkInActive
 
 		_, err = collectionBooking.InsertOne(ctx, booking)
 		if err != nil {
@@ -100,7 +76,7 @@ func CreateBooking(ctx context.Context, booking *Booking) (*Booking, error) {
 			ctx,
 			bson.M{"_id": booking.TimeSlotID},
 			bson.D{
-				{"$set", bson.D{{"tStatus", Active}}},
+				{"$set", bson.D{{"tStatus", tsActive}}},
 				{"$set", bson.D{{"updatedAt", time.Now().UTC()}}},
 			})
 		if err != nil || data.ModifiedCount == 0 {
